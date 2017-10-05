8c1347b774659961a2188180b49b79d3e4cc2f15
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2017-10-04 21:47:52 -0500

    Update

--- Homework3-Hangman-Game/assets/javascript/game.js
+++ Homework3-Hangman-Game/assets/javascript/game.js
@@ -1,4 +1,4 @@
-var newWords = ["ace", "doubles", "singles", "fault", "backhand", "forehand", "serve", "volley", "love", "match", "set", "breakpoint"];
+var newWords = ["ace", "doubles", "singles", "fault", "backhand", "forehand", "serve", "volley", "love", "match", "set", "breakpoint", "racquet"];
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
@@ -7,7 +7,7 @@ var unsolvedWordSplit = [];
 var unsolvedWordBlank = [];
 var currentWord = document.getElementById("current-word");
 var wins = document.getElementById("wins");
-
+var winsCount = 0;
 var remainingGuesses = document.getElementById("guesses-remaining");
 
 var lettersGuessed = document.getElementById("guessed-letters");
@@ -15,6 +15,11 @@ var lettersGuessedArray = [];
 
 //assigns a random word to the current unvoledWord for that round and creates an array of the word, and an array with the blank letters
 function randomWord(){
+	unsolvedWordBlank = [];
+	lettersGuessedArray = [];
+	lettersGuessed.textContent = lettersGuessedArray.join(", ");
+	remainingGuessesCount = 10;
+	remainingGuesses.textContent = remainingGuessesCount;
 	unsolvedWord = newWords[Math.floor(Math.random() * newWords.length)];
 	unsolvedWordSplit = unsolvedWord.split("");
 	for (i=0; i < unsolvedWordSplit.length; i++) {
@@ -23,8 +28,11 @@ function randomWord(){
 	currentWord.textContent = unsolvedWordBlank.join(" ");
 }
 
+
+
 //function to check whether a letter guessed is within the word, and to replace the blanks with that letter. 
 function letterCheck(key) {
+
 	for (var i = 0; i < unsolvedWordSplit.length; i++){
 		if (unsolvedWordSplit[i] === key) {
 			unsolvedWordBlank[i] = key;
@@ -36,10 +44,10 @@ function letterCheck(key) {
 		currentWord.textContent = unsolvedWordBlank.join(" ");
 		}
 
-
+	var letterUsed = lettersGuessedArray.indexOf(key);
 
 	for (var j=0; j<letters.length; j++) {
-		if (letters[j]===key){
+		if (letters[j]===key && letterUsed === -1){
 			lettersGuessedArray.push(key);
 			lettersGuessed.textContent = lettersGuessedArray.join(", ");
 		}
@@ -47,14 +55,30 @@ function letterCheck(key) {
 
 	// Add an if statement here, that will check to see where the key is, if it retuns a -1 (not in the array), then have the guesses number go down by one
 
+	var guessedLetter = unsolvedWordSplit.indexOf(key);
+	var letterIncl = letters.indexOf(key);
 
+	if (guessedLetter === -1 && letterUsed === -1 && letterIncl != -1){
+		remainingGuessesCount--;
+		remainingGuesses.textContent = remainingGuessesCount;
+	}	
+
+	if (remainingGuessesCount < 0) {
+		alert("You Lose! Start Over");
+		randomWord();
+
+	}
 
 	// add a statement here to check and see if there are anymore _ values in the array. If it returns a -1, have the wins number go up by one, and alert the player that they won
 	var stillBlank = unsolvedWordBlank.indexOf("_");
 
 	if (stillBlank === -1){
 		alert("You Won!");
+		winsCount ++;
+		wins.textContent = winsCount;
+		randomWord();
 	}
+
 
 }
 
