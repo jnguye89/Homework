dee90d8978ddf24961d51db79fb1271c0289ad30
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2017-10-15 12:20:16 -0500

    Update

---
+++ Homework4/Star Wars RPG/assets/javascript/game.js
@@ -0,0 +1,24 @@
+var obiWanKanobi = {
+	healthPoints: 120,
+	attackPower: 8,
+	counterAttackPoints: , 
+};
+
+
+var darthMaul = {
+	healthPoints: 180,
+	attackPower: ,
+	counterAttackPoints: 25,
+};
+
+var lukeSkywalker = {
+	healthPoints: 100,
+	attackPower: ,
+	counterAttackPoints: 5,
+};
+
+var darthSidious = {
+	healthPoints: 150,
+	attackPower: ,
+	counterAttackPoints: 20,
+};
